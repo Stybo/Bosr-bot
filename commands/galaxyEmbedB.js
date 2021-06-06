b8735980ab18273c8cab71f60aaed787df48a09c
Stybo <Stenbos05@gmail.com>
2021-06-06 12:28:26 +0200

    .

---
+++ commands/galaxyEmbedB.js
@@ -0,0 +1,25 @@
+const discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+
+    var botEmbed = new discord.MessageEmbed()
+        .setTitle("Galxy Muismat (Blauw)")
+        .setDescription(`**Prijs:** 
+        €26,99
+        
+        **Grootte:**
+        900 x 400 x 4mm
+        
+        **Kenmerken:**
+        Polyester (Bovenkant)
+        Antislip & milieuvriendelijk rubber (onderzijde)
+        Gestikte randen
+        Machinewasbaar (koud)`)
+        .setColor("#0d0041")
+        .setImage('https://cdn.discordapp.com/attachments/841371133264658474/851042455246602260/Galaxy_illustration_blauw_webshop.png')
+
+    return message.channel.send(botEmbed);
+}
+module.exports.help = {
+    name: "galaxyEmbedB"
+}
